c901a8a0ba172e00bd3270ae956582d13d96229c
Stefan <7stefanadrian@gmail.com>
2023-03-17 19:59:45 +0200

    setup completed and added an item - completed setup -created a custom item and addded to the game

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,25 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
+import net.minecraft.util.registry.Registry;
+
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	//add an item
+	public static final Item EXAMPLE_ITEM = new Item(new FabricItemSettings());
+
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +28,8 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("modid", "example_item"), EXAMPLE_ITEM);
 	}
+
+
 }
